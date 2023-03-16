bd035f9d9f16f518636603ea4251acc2830230ca
Айдос Аманов <a.taimaganbetov02@mail.ru>
2023-03-16 18:52:39 +0600

    second commit

--- app/src/main/java/com/example/superheroes/ViewModel.kt
+++ app/src/main/java/com/example/superheroes/ViewModel.kt
@@ -4,13 +4,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class ViewModel {
-    val model = Model()
 
-    private var _title = MutableStateFlow("")
-    private var _description = MutableStateFlow("")
-
-    val title = _title.asStateFlow()
-    val description = _description.asStateFlow()
 
 
 }
